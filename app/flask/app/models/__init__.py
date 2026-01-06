b3d2a81218d8468650e963da34c6d8a6d7290ff7
Deuky <ygustin@gmail.com>
2026-01-06 16:42:42 +0100

    Feature/pipeline (#1)
    [CHIFT-API] Pipeline and Infra

--- app/flask/app/models/__init__.py
+++
@@ -1,4 +0,0 @@
-from .contact import Contact
-from .res_partner_audit import ResPartnerAudit
-
-__all__ = ["Contact", "ResPartnerAudit"]
--- scheduling/entrypoint.sh
+++ scheduling/entrypoint.sh
@@ -1,7 +1,6 @@
 #!/bin/bash
 
 make force-stop
-make install
 make start
 
 tail -f /var/log/cron.log
